3baf4077fa255c5824956c6a228415a5a7267dd0
KelvinMvungi <kelvin.mvungi01@gmail.com>
2023-12-11 17:08:15 +0300

    Add a function to compare digit words

--- 2023/day_1/part_2/sum.c
+++ 2023/day_1/part_2/sum.c
@@ -40,7 +40,20 @@ Position max_position(Position a, Position b) {
         return a;
     } else if (a.index > b.index) {
         return a;
-    } esle {
+    } else {
         return b;
     }
+}
+
+int is_named_int (char* str) {
+    if (strncmp(str, "one", 3) == 0) return 1;
+    if (strncmp(str, "two", 3) == 0) return 2;
+    if (strncmp(str, "three", 5) == 0) return 3;
+    if (strncmp(str, "four", 4) == 0) return 4;
+    if (strncmp(str, "five", 4) == 0) return 5;
+    if (strncmp(str, "six", 3) == 0) return 6;
+    if (strncmp(str, "seven", 5) == 0) return 7;
+    if (strncmp(str, "eight", 5) == 0) return 8;
+    if (strncmp(str, "nine", 4) == 0) return 9;
+    return -1;
 }
