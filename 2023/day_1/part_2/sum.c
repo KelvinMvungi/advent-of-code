4e4ace2325450ca11f388a2dd8f45c7bfa05735c
KelvinMvungi <kelvin.mvungi01@gmail.com>
2023-12-11 16:33:38 +0300

    Add a custom Position data structure and function

--- 2023/day_1/part_2/sum.c
+++ 2023/day_1/part_2/sum.c
@@ -12,5 +12,35 @@ int parse_digit(char ch) {
         return ch - '0';
 }
 
+typedef struct {
+    int index;
+    int value;
+} Position;
 
+Position min_position(Position a, Position b) {
+    if (a.index == -1 && b.index == -1) {
+        return b;
+    } else if (a.index == -1) {
+        return b;
+    } else if (b.index == -1) {
+        return a;
+    } else if (a.index < b.index) {
+        return a;
+    } else {
+        return b;
+    }
+}
 
+Position max_position(Position a, Position b) {
+    if(a.index == -1 && b.index == -1) {
+        return b;
+    } else if(a.index == -1) {
+        return b;
+    } else if(b.index == -1){
+        return a;
+    } else if (a.index > b.index) {
+        return a;
+    } esle {
+        return b;
+    }
+}
